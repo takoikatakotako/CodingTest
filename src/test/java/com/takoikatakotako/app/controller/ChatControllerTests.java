b649e8b098e5d1b4a412fe864b378209039f432f
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2024-04-21 18:36:59 +0900

    1on1でメッセージを取得できるコードを作成

--- src/test/java/com/takoikatakotako/app/controller/ChatControllerTests.java
+++ src/test/java/com/takoikatakotako/app/controller/ChatControllerTests.java
@@ -2,10 +2,7 @@ package com.takoikatakotako.app.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.takoikatakotako.app.entity.ChatRoomCreateRequestEntity;
-import com.takoikatakotako.app.entity.ChatRoomResponseEntity;
-import com.takoikatakotako.app.entity.UserResponseEntity;
-import com.takoikatakotako.app.entity.UserSignUpRequestEntity;
+import com.takoikatakotako.app.entity.*;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,69 +41,28 @@ public class ChatControllerTests {
         UserResponseEntity secondUserSignUpResponse = signupUser(secondUserName);
         Long secondUserID = secondUserSignUpResponse.getId();
 
-
         // チャットルーム作成
-        ChatRoomCreateRequestEntity chatRoomCreateRequestEntity = new ChatRoomCreateRequestEntity();
-        chatRoomCreateRequestEntity.setType("XXXXX");
         ArrayList<Long> userIDList = new ArrayList<>();
         userIDList.add(firstUserID);
         userIDList.add(secondUserID);
-        chatRoomCreateRequestEntity.setUsers(userIDList);
-
-        ObjectMapper mapper = new ObjectMapper();
-        String chatRoomCreateRequestJSON = mapper.writeValueAsString(chatRoomCreateRequestEntity);
-
-        MvcResult chatRoomCreateRequestResult = mockMvc.perform(
-                        post("/chat/room/create")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(chatRoomCreateRequestJSON)
-                )
-                .andExpect(status().is2xxSuccessful())
-                .andReturn();
-
-        String chatRoomCreateResponse = chatRoomCreateRequestResult.getResponse().getContentAsString();
-        ChatRoomResponseEntity chatRoomResponse = mapper.readValue(chatRoomCreateResponse, ChatRoomResponseEntity.class);
-
+        ChatRoomResponseEntity chatRoomResponse = createChatRoom("DIRECT", userIDList);
+        Long chatRoomID = chatRoomResponse.getChatRoomID();
 
         // メッセージ送信
+        postMessage(chatRoomID, firstUserID, "こんにちは");
+        postMessage(chatRoomID, secondUserID, "こんにちは!");
+        postMessage(chatRoomID, firstUserID, "いい天気ですね");
+        postMessage(chatRoomID, secondUserID, "そうですね〜");
 
-//        UserSignUpRequestEntity signupRequest = new UserSignUpRequestEntity();
-//        signupRequest.setName(userName);
-//
-//        ObjectMapper mapper = new ObjectMapper();
-//        String signupRequestJSON = mapper.writeValueAsString(signupRequest);
-//
-//        MvcResult signupRequestResult = mockMvc.perform(
-//                        post("/user/signup")
-//                                .contentType(MediaType.APPLICATION_JSON)
-//                                .content(signupRequestJSON)
-//                )
-//                .andExpect(status().is2xxSuccessful())
-//                .andReturn();
-//
-//        String signupRequestResponse = signupRequestResult.getResponse().getContentAsString();
-//        UserResponseEntity signupResponse = mapper.readValue(signupRequestResponse, UserResponseEntity.class);
-//
-//        assertEquals(userName, signupResponse.getName());
-//
-//
-//        // Get User
-//        Long userID = signupResponse.getId();
-//        MvcResult getUserResult = mockMvc.perform(
-//                        get("/user/" + userID.toString())
-//                                .contentType(MediaType.APPLICATION_JSON)
-//                )
-//                .andExpect(status().is2xxSuccessful())
-//                .andReturn();
-//
-//        String getUserRequestResponse = signupRequestResult.getResponse().getContentAsString();
-//        UserResponseEntity getUserResponse = mapper.readValue(getUserRequestResponse, UserResponseEntity.class);
-//
-//        assertEquals(userID, getUserResponse.getId());
-//        assertEquals(userName, getUserResponse.getName());
+        //
+        ChatRoomMessageListResponseEntity xxx = getChatRoomMessageList(chatRoomID);
+        assertEquals(xxx.getMessages().size(), 4);
     }
 
 
+    /**
+     * ユーザー作成
+     */
     private UserResponseEntity signupUser(String userName) throws Exception {
         UserSignUpRequestEntity signupRequest = new UserSignUpRequestEntity();
         signupRequest.setName(userName);
@@ -124,5 +80,78 @@ public class ChatControllerTests {
 
         String signupRequestResponse = signupRequestResult.getResponse().getContentAsString();
         return mapper.readValue(signupRequestResponse, UserResponseEntity.class);
+    }
+
+
+    /**
+     * チャットルーム作成
+     */
+    private ChatRoomResponseEntity createChatRoom(String type, ArrayList<Long> users) throws Exception {
+        ChatRoomCreateRequestEntity chatRoomCreateRequestEntity = new ChatRoomCreateRequestEntity();
+        chatRoomCreateRequestEntity.setType(type);
+        chatRoomCreateRequestEntity.setUsers(users);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String chatRoomCreateRequestJSON = mapper.writeValueAsString(chatRoomCreateRequestEntity);
+
+        MvcResult chatRoomCreateRequestResult = mockMvc.perform(
+                        post("/chat/room/create")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(chatRoomCreateRequestJSON)
+                )
+                .andExpect(status().is2xxSuccessful())
+                .andReturn();
+
+        String chatRoomCreateResponse = chatRoomCreateRequestResult.getResponse().getContentAsString();
+        return mapper.readValue(chatRoomCreateResponse, ChatRoomResponseEntity.class);
+    }
+
+
+
+
+    /**
+     * チャット投稿
+     */
+    private String postMessage(Long chatRoomID, Long userID, String message) throws Exception {
+        ChatPostMessageRequestEntity requestEntity = new ChatPostMessageRequestEntity();
+        requestEntity.setUserID(userID);
+        requestEntity.setMessage(message);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String requestEntityJSON = mapper.writeValueAsString(requestEntity);
+
+        MvcResult requestResult = mockMvc.perform(
+                        post("/chat/room/" + chatRoomID.toString() + "/post")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestEntityJSON)
+                )
+                .andExpect(status().is2xxSuccessful())
+                .andReturn();
+
+        return requestResult.getResponse().getContentAsString();
+        // String response = requestResult.getResponse().getContentAsString();
+        // return mapper.readValue(response, String.class);
+    }
+
+
+
+
+
+
+    /**
+     * チャット取得
+     */
+    private ChatRoomMessageListResponseEntity getChatRoomMessageList(Long chatRoomID) throws Exception {
+        MvcResult requestResult = mockMvc.perform(
+                        get("/chat/room/" + chatRoomID.toString() + "/message")
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().is2xxSuccessful())
+                .andReturn();
+
+        // return requestResult.getResponse().getContentAsString();
+         String response = requestResult.getResponse().getContentAsString();
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(response, ChatRoomMessageListResponseEntity.class);
     }
 }
